d66146e0c18de8c9647a0d295918d7441a091daa
Prince Ugwuh <prince.ugwuh@gmail.com>
2019-08-17 18:13:08 -0500

    updated package.swift file

--- Package.swift
+++ Package.swift
@@ -24,9 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NetworkHijack",
-            dependencies: []),
+            dependencies: [],
+            path: "Network Hijack"),
         .testTarget(
             name: "Network HijackTests",
-            dependencies: ["NetworkHijack"]),
+            dependencies: ["NetworkHijack"],
+            path: "Network HijackTests"),
     ]
 )
